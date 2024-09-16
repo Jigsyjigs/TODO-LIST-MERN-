378af4eff8b589c1c9ebb886107161ed0852fab8
Jigs <jigsdominic.ipong@outlook.com>
2024-09-16 10:56:12 +0800

    Initial commit

---
+++ Server/index.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import TodoModel from './Models/Todo.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://jigs:yssa1@cluster0.9vpud.mongodb.net/todolist?retryWrites=true&w=majority&appName=Cluster0')
+
+app.get('/get', (req,res) => {
+    TodoModel.find()
+    .sort ({createdAt: -1})
+    .then (result => res.json(result))
+    .catch (err => res.json(err))
+})
+
+app.put('/update/:id', (req,res) => {
+    const {id} = req.params;
+    TodoModel.findByIdAndUpdate({_id: id}, {done: true})
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+app.post('/add', (req,res) => {
+    const task = req.body.task;
+    TodoModel.create({
+        task: task
+    }) .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+app.delete('/delete/:id', (req,res) => {
+    const {id} = req.params;
+    TodoModel.findByIdAndDelete({_id: id})
+    .then (result => res.json(result))
+    .catch (err => res.json(err))
+})
+
+app.listen(3001, () => {
+    console.log("Server is running")
+})
